7f16244ae7f0e6a176f5ca8a65c2c7a34772969e
peter.shin.sdsc <peter.shin.sdsc@98350c0c-5147-0410-9de4-f1fe730f28bb>
2008-07-02 18:05:00 +0000

    added option of specifying the xml file location

--- apps/oss-apps/src/edu/ucsd/osdt/db/dt2db.java
+++ apps/oss-apps/src/edu/ucsd/osdt/db/dt2db.java
@@ -4,10 +4,33 @@ import edu.ucsd.osdt.db.rbnb.*;
 
 public class dt2db {
 	
+	
 	public static void main (String args[]) {
+		
+		String filePath = null;
+		
+		if (args.length > 0) {
+			filePath = args[0];
+			dt2db d2d = new dt2db();
+			d2d.exec(filePath);
+			
+		}
+		else 
 		(new dt2db()).exec2();
 		
 	}
+	
+	public void exec(String fp) {
+		String inputFileName =  fp;
+		Config aConfig = ConfigUtil.readConfig( inputFileName );
+		
+		SinkClientManager sMan = new SinkClientManager();
+		sMan.setConfig(aConfig);
+		sMan.exec();
+		
+		
+	}
+	
 	
 	public void exec2() {
 		String inputFileName =  "/Users/petershin/Downloads/config-parser/config.xml";
